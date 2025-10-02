bae31f31f68b08ec5c0e93ffcdf5f7995d9da00a
Remo Grillo <grilloremo@gmail.com>
2025-10-02 11:24:07 +0200

    favicon img router online

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@
 import './globals.css'
 // 1. RIMOSSA l'importazione da 'next/font/google'
 import { satoshi, roboto_mono } from './fonts' // Manteniamo solo l'importazione del font locale
+import { getImagePath } from './utils/getImagePath'
 
 // 2. RIMOSSA la definizione della costante 'sequel'
 
@@ -12,6 +13,8 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }) {
+  const faviconPath = getImagePath('/images/favicon.svg');
+  
   return (
     // 3. AGGIUNTA la variabile del font al tag <html>
     // Usiamo un template literal `` per combinare le classi
@@ -19,7 +22,11 @@ export default function RootLayout({ children }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/images/favicon.svg" type="image/svg+xml" />
+        <link 
+          rel="icon" 
+          href={faviconPath} 
+          type="image/svg+xml" 
+        />
       </head>
       {/* 4. RIMOSSA la classe 'font-sequel' dal body */}
       <body className="antialiased text-foreground">
